9dd7298e0c5188937384566b8616bc06a32b59d7
Justin Jones <justinjones53@gmail.com>
2019-06-27 23:57:56 -0700

    adding more to schema

--- schema.js
+++ schema.js
@@ -2,10 +2,49 @@ export default {
   'Module': {
     name: 'String!',
     ip: 'String',
-    type: 'ModuleType',
+    chip: 'String',
+    type: 'Type',
+    group: 'Group',
   },
-  'ModuleType': {
+
+  'Type': {
     name: 'String!',
     label: 'String!',
+    modules: '[Module]',
+  },
+
+  'Group': {
+    name: 'String!',
+    label: 'String!',
+    modules: '[Module]',
+  },
+
+  'Event': {
+    module: 'Module!',
+    attribute: 'String!',
+    value: 'String!',
+    previous: 'String',
+  },
+
+  'Rule': {
+    name: 'String!',
+    operator: 'String!', // default '&&'
+    conditions: '[Condition]',
+    actions: '[Action]',
+  },
+
+  'Condition': {
+    rule: 'Rule!',
+    module: 'Module!',
+    attribute: 'String!',
+    operator: 'String!', // enum ['==', '!=', '>', '<', '> x <']
+    value: 'String!',
+  },
+
+  'Action': {
+    rule: 'Rule!',
+    module: 'Module!',
+    method: 'String!',
+    arguments: '[String]',
   },
 }
